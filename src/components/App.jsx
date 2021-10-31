233440bfdd883e8e4cdf239aeb4083ca4fb2586d
vtorresm <vatorresm88@gmail.com>
2021-10-31 10:54:15 -0500

    Añadiendo CSS y Sass

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import '../styles/global.scss';
+
 const App = () => {
   return <h1>Hola mundo...!</h1>;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -27,6 +28,14 @@ module.exports = {
 						loader: 'html-loader'
 					}
 				]
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader",
+				],
 			}
 		]
 	},
@@ -35,5 +44,14 @@ module.exports = {
 			template: './public/index.html',
 			filename: './index.html'
 		}),
-	]
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+	],
+	devServer: {
+		//contentBase: path.join(__dirname, 'dist'),
+		allowedHosts: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 3000,
+	}
 }
