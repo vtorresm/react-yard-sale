f9b0f904f852a2835970c85061c848aad058437f
vtorresm <vatorresm88@gmail.com>
2021-10-31 15:15:34 -0500

    userEffect y consumo de APIs

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 import ProductItem from '../components/ProductItem';
 
 import '../styles/ProductList.scss';
 
+const API = 'https://api.escuelajs.co/api/v1/products';
+
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(async () => {
+    const response = await axios.get(API);
+    setProducts(response.data);
+  }, []);
+
   return (
     <section className='main-container'>
       <div className='ProductList'>
-        <ProductItem />
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
       </div>
     </section>
   );
