86fc8d15e86bf2d624966863399e93a8202209f7
vtorresm <vatorresm88@gmail.com>
2021-10-31 11:49:43 -0500

    Agregando componente Recovery Password

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
+import RecoveryPassword from '../containers/RecoveryPassword';
 
 import '../styles/global.css';
 
@@ -9,6 +10,7 @@ const App = () => {
   return (
     <Layout>
       <Login />
+      <RecoveryPassword />
     </Layout>
   );
 };
