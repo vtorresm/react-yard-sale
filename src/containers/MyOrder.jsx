e275bb2baa5f37498c4b4d7a8e599274b1d664c6
Victor Alberto Torres Mori <vtorremo@everis.com>
2021-11-02 20:59:11 -0500

    Actualizando las rutas import de containers

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 
-import AppContext from '../context/AppContext';
+import AppContext from '@context/AppContext';
 import OrderItem from '@components/OrderItem';
 
 import '@styles/MyOrder.scss';
